0fb1d964c87b1fcf6982eeb300cf71ba1a197e8b
missivaeak <adamesbj@gmail.com>
2025-10-23 20:23:49 +0200

    Change inactive border colour to be easier to distinguish from active borders

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -4,13 +4,15 @@ import "./input-text.css"
 type InputTextProps<T extends string = string> = {
   value: T;
   onChange: (change: T) => void;
+  className?: string;
+  maxSize?: number;
 }
 
-export default function InputText({ value, onChange }: InputTextProps) {
+export default function InputText({ value, onChange, className, maxSize }: InputTextProps) {
   function onChangeHtml(event: ChangeEvent<HTMLInputElement>) {
     const change = event.target.value;
     onChange(change);
   }
 
-  return <input className="input-text" type="text" value={value} onChange={onChangeHtml} />
+  return <input className={`input-text${className ? ` ${className}` : ''}`} size={maxSize} maxLength={maxSize} type="text" value={value} onChange={onChangeHtml} />
 }
